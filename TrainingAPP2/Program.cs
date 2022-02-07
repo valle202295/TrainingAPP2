549e2c0ac0e66b5ec90d740e8ae4e085ca3d24fb
ogcsm <ogcsm@BUILDTEST1>
2022-02-07 14:56:00 +0100

    Add project files.

---
+++ TrainingAPP2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TrainingAPP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Code block of Main method
+
+            Console.WriteLine("Hello World!");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello" + userName);
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            if (userAge > 17)
+            {
+                Console.WriteLine("Do you want a Beer?");
+            }
+
+
+
+        }//End of Main method
+    }
+
+
+  
+
